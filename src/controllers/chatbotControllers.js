e2900a0c82bb9d6a9320acc76ce334e870582215
minhngu123 <m31082002@gmail.com>
2024-09-05 16:14:06 +0700

    update fix

--- src/controllers/chatbotControllers.js
+++ src/controllers/chatbotControllers.js
@@ -47,11 +47,11 @@ function handleMessage(sender_psid, received_message){
 
 }
 //Handles messaging_postbacks events
-function handleMessage(sender_psid, received_postback){
+function handlePostback(sender_psid, received_postback){
     
 }
 //Sends response messages via the Send API
-function handleMessage(sender_psid, received_response){
+function callSendApi(sender_psid, received_response){
     
 }
 
